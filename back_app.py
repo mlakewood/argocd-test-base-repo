5bc1744c33ddb2bc184ea79ee10e3215fc98d76a
Mark Lakewood <underplank@gmail.com>
2022-12-28 13:34:50 +0800

    Split kustomize workflow

--- back_app.py
+++ back_app.py
@@ -1,7 +1,38 @@
 from flask import Flask
+import requests
+import environ
+import structlog
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    BatchSpanProcessor,
+)
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 
+# Set OpenTelemetry Resource Attributes
+resource = Resource(attributes={
+    "service.name": "back-app"
+})
+
+# Configure OpenTelemetry Tracer and Exporter
+trace.set_tracer_provider(TracerProvider(resource=resource))
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter(insecure=True, timeout=1))
+)
+tracer = trace.get_tracer(__name__)
+
+
+logger = structlog.get_logger()
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
+def hello_other():
+    return "Other"
 
 @app.route("/message")
 def hello_world():
--- front_app.py
+++ front_app.py
@@ -2,15 +2,38 @@ from flask import Flask
 import requests
 import environ
 import structlog
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    BatchSpanProcessor,
+)
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+# Set OpenTelemetry Resource Attributes
+resource = Resource(attributes={
+    "service.name": "front-app"
+})
+
+# Configure OpenTelemetry Tracer and Exporter
+trace.set_tracer_provider(TracerProvider(resource=resource))
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(OTLPSpanExporter(insecure=True, timeout=1))
+)
+tracer = trace.get_tracer(__name__)
 
 logger = structlog.get_logger()
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
 
 @environ.config
 class AppConfig:
     back_app_host = environ.var(help="This is the back_app hostname")
     back_app_port = environ.var(help="This is the back_app port")
-
 
 @app.route("/health")
 def health():
